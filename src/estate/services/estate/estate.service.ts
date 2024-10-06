6789d83cad48721321dc55d977761dd6f5aa70bb
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-06 21:37:08 +0900

    不動産取引価格APIを叩いて情報を取得

--- src/estate/services/estate/estate.service.ts
+++ src/estate/services/estate/estate.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { EstateQueryDto } from 'src/estate/dto/estate-query.dto';
+import axios from 'axios';
 
 @Injectable()
-export class EstateService {}
+export class EstateService {
+  async getEstateTransaction(query: EstateQueryDto) {
+    const { year, prefCode, cityCode, displayType } = query;
+    const url = `https://opendata.resas-portal.go.jp/api/v1/townPlanning/estateTransaction/bar?year=${year}&prefCode=${prefCode}&cityCode=${cityCode}&displayType=${displayType}`;
+    const headers = { 'X-API-KEY': process.env.RESAS_API_KEY };
+
+    const response = await axios.get(url, { headers });
+    console.log('Response:', response.status, response.data);
+    return response.data;
+  }
+}
