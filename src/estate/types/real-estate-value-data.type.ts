1bcfb0f1f7a7b1e8f2576dd65708f522663a1d58
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-09 01:02:26 +0900

    リントエラーを修正

--- src/estate/types/real-estate-value-data.type.ts
+++ src/estate/types/real-estate-value-data.type.ts
@@ -29,4 +29,7 @@ export interface ServerErrorResponse {
   message: string;
 }
 
-export type RealEstateValueData = SuccessResponse | ValidationErrorResponse | ServerErrorResponse;
+export type RealEstateValueData =
+  | SuccessResponse
+  | ValidationErrorResponse
+  | ServerErrorResponse;
