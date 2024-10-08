5185c89864daa7999031ebadb002fc6499b1586d
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-09 00:59:01 +0900

    依存関係を修正

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { RealEstateController } from './estate/controllers/estate/realestate.controller';
-import { EstateService } from './estate/services/estate/estate.service';
 import { EstateModule } from './estate/estate.module';
 
 @Module({
   imports: [EstateModule],
-  controllers: [AppController, RealEstateController],
-  providers: [AppService, EstateService],
 })
 export class AppModule {}
