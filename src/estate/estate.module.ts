71d0527664c9df621d1e7543a261ac173e961d43
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-10 01:27:39 +0900

    ディレクトリの階層が間違っていたため修正

--- src/estate/estate.module.ts
+++ src/estate/estate.module.ts
@@ -1,6 +1,6 @@
 // src/estate/estate.module.ts
 import { Module } from '@nestjs/common';
-import { RealEstateController } from './controllers/estate/realestate.controller';
+import { RealEstateController } from './controllers/realestate.controller';
 import { InquireRealEstateValueUseCase } from './use-cases/inquire-real-estate-value.use-case';
 import { RealEstateDataSourceRepository } from './repositories/real-estate-data-source.repository';
 
