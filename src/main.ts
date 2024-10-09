0329fc5dd2e3538e3bc965599bc6c6a4121b583b
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-09 19:38:09 +0900

    リント設定変更による修正

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,25 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe, BadRequestException } from '@nestjs/common';
 
-
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ 
-    transform: true,
-    exceptionFactory: (errors) => {
-      const messages = errors.map(
-        (error) => ({
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors) => {
+        const messages = errors.map((error) => ({
           field: error.property,
           errors: Object.values(error.constraints),
-        })
-      );
-      return new BadRequestException({
-        statusCode: 400,
-        message: messages,
-      });
-    }
-  }));
+        }));
+        return new BadRequestException({
+          statusCode: 400,
+          message: messages,
+        });
+      },
+    }),
+  );
 
   await app.listen(3000);
 }
